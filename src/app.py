c6c923da0788e9714de338a5ddbd40f49346e4c6
andrewsthomasj <andrewsthomasj+github@gmail.com>
2021-07-21 21:47:33 -0700

    Update ports/private-ip so I can run in parallel with unforked version

--- src/app.py
+++ src/app.py
@@ -68,4 +68,4 @@ def api():
     return jsonify(context)
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=int(os.environ.get('PORT', '8000')))
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=int(os.environ.get('PORT', '8001')))
