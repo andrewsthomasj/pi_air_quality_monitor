ef5fbb511343aee3d80d6a73099b1b2c84d7d360
andrewsthomasj <andrewsthomasj+github@gmail.com>
2021-07-22 23:41:54 -0700

    fix variable

--- src/AirQualityMonitor.py
+++ src/AirQualityMonitor.py
@@ -26,8 +26,8 @@ class AirQualityMonitor():
     def get_average_value(self, granularity, n):
         """returns the average value from the last 'n' datapoints at 'granularity'"""
         list = self.get_historic_values(granularity, n)
-        list_pm10 = [x['measurement']['pm10'] for x in measurement]
-        list_pm2_5 = [x['measurement']['pm2.5'] for x in measurement]
+        list_pm10 = [x['measurement']['pm10'] for x in list]
+        list_pm2_5 = [x['measurement']['pm2.5'] for x in list]
         return list_pm10
         #avg_pm10 = sum(list_pm10) / len(list_pm10)
         #avg_pm2_5 = sum(list_pm2_5) / len(list_pm2_5)
